a9efbcd292d558a5713c54d298b76a9207b691bc
zhangxun <zhxun0422@gmail.com>
2015-09-20 15:07:05 +0800

    求后面的数和前面的数的最大差值题的修改

--- openjudge/MaxDiff/MaxDiff.cpp
+++ openjudge/MaxDiff/MaxDiff.cpp
@@ -13,21 +13,21 @@
 2
 3 1
 输出：
-0
+-2
 
 【TestCase3】
 输入：
 7
 8 4 9 5 3 7 1
 输出：
-0
+4
 
 【TestCase4】
 输入：
 1
 0
 输出：
-0
+input error
 
 【TestCase5】
 输入：
@@ -38,14 +38,16 @@
 */
 /************************************************************************/
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
 int Array[1000];
 int getMaxDiff(int* a,int length)
 {
+    assert(length > 1);
 	int minNum = a[0];
-	int Sub_MaxDiff = 0;
-	for (int i = 1; i < length; i++)
+    int Sub_MaxDiff = a[1] - a[0];
+	for (int i = 2; i < length; i++)
 	{
 		minNum = (a[i] < minNum) ? a[i] : minNum;
 		Sub_MaxDiff = ((a[i] - minNum)>Sub_MaxDiff)? (a[i] - minNum) : Sub_MaxDiff;
@@ -54,9 +56,13 @@ int getMaxDiff(int* a,int length)
 }
 int main()
 {
-
     int N = 0;//输入序列的长度
 	cin >> N;
+    if (N < 2)
+    {
+        cout << "input error";
+        return -1;
+    }
 	for (int i = 0; i < N; i++)
 	{
 		cin >> Array[i]; 
